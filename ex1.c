49a4c5f48e776eaf6e4873a98ea7bf20d822ef6f
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2021-03-20 22:34:25 +0200

    First update

--- ex1.c
+++ ex1.c
@@ -1,37 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <malloc.h>
+#include <stdlib.h>
 
-int getCommand(char* command);
+int getCommand();
 int isBuiltIn(char* command);
-void handleBuiltIn(char* command);
+void handleBuiltIn(char* command[], size_t numOfArg);
+void freeCommand();
+
+void handleCommandJobs(char command[][100], size_t numOfArg);
+void handleCommandHistory(char command[][100], size_t numOfArg);
+void handleCommandCd(char command[][100], size_t numOfArg);
+void handleCommandExit(char command[][100], size_t numOfArg);
+
 int stop = 0;
+char* listCommand[100];
 
-int main() {
+int main(int argc, char **argv) {
 
+    atexit(freeCommand);
 
     while (!stop) {
+        char *args[] = {"ls", "-r", "-t", "-l", NULL };
+        //int s = execvp("ls", args);
+        //printf("%d\n", s);
 
         printf("$ ");
-        char command[100];
-        bzero(command, 100);
-        getCommand(command);
+        fflush(stdout);
+        bzero(listCommand, 100);
 
-        if (isBuiltIn(command)) {
-            handleBuiltIn(command);
+        //char * l = *list;
+
+
+        int numOfArg = getCommand();
+        //int s = execvp(listCommand[0], listCommand);
+
+        /**********/
+        //stop = 1;
+        /**********/
+
+        if (isBuiltIn(listCommand[0])) {
+            handleBuiltIn(listCommand, numOfArg);
         } else {
-            int status = fork();
-            if (status < 1) {
+            //execvp(listCommand[0],listCommand);
+            pid_t pid;
+
+            if ((pid = fork())==0) {
+                // the child process
+                int s = execvp(listCommand[0], listCommand);
+                if (s<0)
+                    printf("exec failed\n");
+            } else if (pid < 0) {
                 // if the command fork failed
                 printf("fork failed\n");
-            } else if (status == 0) {
+            } else {
                 // the father process
                 // check if it is foreground or background
-
-            } else {
-                // the son process
-                // go to the head of the loop and get a new command
-                continue;
+                usleep(100000);
             }
         }
     }
@@ -39,16 +65,26 @@ int main() {
     return 0;
 }
 
-int getCommand(char* command) {
+int getCommand() {
+
+    char command[100];
+    // preparing to get a command
     char* commandSplit;
-    scanf("%[^\n]s", command);
-    commandSplit = strtok(command, " ");
+    // getting a command
+    scanf(" %[^\n]s", command);
+    // split into words
     int i = 0;
+    commandSplit = strtok(command, " ");
     while (commandSplit != NULL) {
-        commandSplit = strtok (NULL, " ");
+
+        listCommand[i] = malloc(strlen((commandSplit+1)) * sizeof(char));
+        strcpy(listCommand[i], commandSplit);
+        printf("%s\n", commandSplit);
+        commandSplit = strtok(NULL, " ");
         i++;
     }
-    return 0;
+    printf("%d\n",i);
+    return i;
 }
 
 int isBuiltIn(char* command) {
@@ -60,6 +96,65 @@ int isBuiltIn(char* command) {
     return 0;
 }
 
-void handleBuiltIn(char* command) {
+void handleBuiltIn(char* command[], size_t numOfArg) {
+    /*if (strcmp(command[0], "jobs") == 0)
+        handleCommandJobs(command, numOfArg);
+    else if (strcmp(command[0], "history") == 0) {
+        handleCommandHistory(command, numOfArg);
+    } else if (strcmp(command[0], "cd") == 0) {
+        handleCommandCd(command, numOfArg);
+    } else {
+        handleCommandExit(command, numOfArg);
+    }*/
 
 }
+
+void handleCommandJobs(char command[][100], size_t numOfArg) {
+
+}
+
+void handleCommandHistory(char command[][100], size_t numOfArg) {
+    printf("%s\n", command[1]);
+
+}
+
+void handleCommandCd(char command[][100], size_t numOfArg) {
+
+}
+
+void handleCommandExit(char command[][100], size_t numOfArg) {
+
+}
+
+
+void freeCommand() {
+    printf("hey\n");
+    int i = 0;
+    while (listCommand[i] != NULL) {
+        free(listCommand[i]);
+    }
+}
+
+
+
+
+/*int getCommand(char listCommand[]) {
+
+    // preparing to get a command
+    char command[100];
+    bzero(command, 100);
+    char* commandSplit;
+    // getting a command
+    scanf("%[^\n]s", listCommand);
+    // split into words
+    int i = 0;
+    commandSplit = strtok(listCommand, " ");
+    while (commandSplit != NULL) {
+        //strcpy(listCommand[i], commandSplit);
+        printf("%s\n", commandSplit);
+        commandSplit = strtok(NULL, " ");
+        i++;
+    }
+    printf("%d\n",i);
+    return i;
+}*/
