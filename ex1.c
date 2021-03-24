7cdab6c5d362fbbbe4e203feff67ef52daadfd61
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2021-03-24 15:02:08 +0200

    The second update.

--- ex1.c
+++ ex1.c
@@ -3,23 +3,33 @@
 #include <unistd.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int getCommand();
 int isBuiltIn(char* command);
 void handleBuiltIn(char* command[], size_t numOfArg);
 void freeCommand();
 
-void handleCommandJobs(char command[][100], size_t numOfArg);
-void handleCommandHistory(char command[][100], size_t numOfArg);
-void handleCommandCd(char command[][100], size_t numOfArg);
-void handleCommandExit(char command[][100], size_t numOfArg);
+void handleCommandJobs(char* command[], size_t numOfArg);
+void handleCommandHistory(char* command[], size_t numOfArg);
+void handleCommandCd(char* command[], size_t numOfArg);
+void handleCommandExit(char* command[], size_t numOfArg);
+
+void handleEcho(char* command[100], size_t numOfArg);
 
 int stop = 0;
 char* listCommand[100];
+char previousPWD[PATH_MAX];
+char currentPWD[PATH_MAX];
 
 int main(int argc, char **argv) {
 
-    atexit(freeCommand);
+    //atexit(freeCommand);
+
+    //long max =
+    //printf("%ld\n", sizeof(previousPWD));
+
+    getcwd(previousPWD, sizeof(previousPWD));
 
     while (!stop) {
         char *args[] = {"ls", "-r", "-t", "-l", NULL };
@@ -35,6 +45,9 @@ int main(int argc, char **argv) {
 
         int numOfArg = getCommand();
         //int s = execvp(listCommand[0], listCommand);
+
+        if (strcmp(listCommand[0], "echo")==0)
+            handleEcho(listCommand, numOfArg);
 
         /**********/
         //stop = 1;
@@ -76,7 +89,6 @@ int getCommand() {
     int i = 0;
     commandSplit = strtok(command, " ");
     while (commandSplit != NULL) {
-
         listCommand[i] = malloc(strlen((commandSplit+1)) * sizeof(char));
         strcpy(listCommand[i], commandSplit);
         printf("%s\n", commandSplit);
@@ -96,8 +108,8 @@ int isBuiltIn(char* command) {
     return 0;
 }
 
-void handleBuiltIn(char* command[], size_t numOfArg) {
-    /*if (strcmp(command[0], "jobs") == 0)
+void handleBuiltIn(char* command[100], size_t numOfArg) {
+    if (strcmp(command[0], "jobs") == 0)
         handleCommandJobs(command, numOfArg);
     else if (strcmp(command[0], "history") == 0) {
         handleCommandHistory(command, numOfArg);
@@ -105,25 +117,41 @@ void handleBuiltIn(char* command[], size_t numOfArg) {
         handleCommandCd(command, numOfArg);
     } else {
         handleCommandExit(command, numOfArg);
-    }*/
+    }
 
 }
 
-void handleCommandJobs(char command[][100], size_t numOfArg) {
+void handleCommandJobs(char* command[100], size_t numOfArg) {
+    system("jobs -p");
 
 }
 
-void handleCommandHistory(char command[][100], size_t numOfArg) {
+void handleCommandHistory(char* command[100], size_t numOfArg) {
     printf("%s\n", command[1]);
 
 }
 
-void handleCommandCd(char command[][100], size_t numOfArg) {
+void handleCommandCd(char* command[100], size_t numOfArg) {
 
+
+
+    if (command[1] != NULL && strcmp(command[1], "-")==0) {
+        chdir(getenv(previousPWD));
+        return;
+    }
+    getcwd(previousPWD, sizeof(previousPWD));
+    if (command[2] != NULL)
+        printf("Too many argument\n");
+    else if (command[1] == NULL || strcmp(command[1], "~")==0)
+        chdir(getenv("HOME"));
+    else if (strcmp(command[1], "..")==0)
+        chdir(command[1]);
+
+    getcwd(currentPWD, sizeof(currentPWD));
 }
 
-void handleCommandExit(char command[][100], size_t numOfArg) {
-
+void handleCommandExit(char* command[100], size_t numOfArg) {
+    exit(0);
 }
 
 
@@ -132,6 +160,14 @@ void freeCommand() {
     int i = 0;
     while (listCommand[i] != NULL) {
         free(listCommand[i]);
+    }
+}
+
+void handleEcho(char* command[100], size_t numOfArg) {
+    char* t;
+    for (int i = 1; i < numOfArg; ++i) {
+        t = strtok(command[i], "\"");
+        strcpy(command[i], t);
     }
 }
 
